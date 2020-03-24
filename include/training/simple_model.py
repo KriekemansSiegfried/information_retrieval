31fd54ae6c72d2f129850a1956b465f6f63c9b59
Pieter-Jan Inghelbrecht <pjinghelbrecht@gmail.com>
2020-03-25 00:18:31 +0100

    added a rank image function to simple model

--- include/training/simple_model.py
+++ include/training/simple_model.py
@@ -1,13 +1,16 @@
+from collections import OrderedDict
+
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import scipy.sparse as sparse
+from scipy import spatial
+# keras
 from sklearn.model_selection import train_test_split
 from tensorflow.keras import optimizers
 from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
 from tensorflow.keras.callbacks import ReduceLROnPlateau
 from tensorflow.keras.layers import Dense, Dropout
-# keras
 from tensorflow.keras.models import Sequential
 
 # %% load data
@@ -22,7 +25,7 @@ df_image = pd.read_csv("include/data/image_features.csv",
 # %% subset captions and image to start with few examples
 num_samples = 2000
 X_captions_subset = df_captions[0:num_samples, :][::5].todense().astype(float)
-y_image_subset = df_image.iloc[0:int(num_samples / 5), 1:].values.astype(float)
+y_image_subset = df_image.iloc[0:int(num_samples / 5), :].values
 
 # make train and validation set (test set is for later once we have found good parameters)
 val_size = round(num_samples / 5 * 0.25)
@@ -60,22 +63,22 @@ reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.2,
 callbacks = [EarlyStopping(monitor='val_loss', patience=20),
              ModelCheckpoint('test_model', monitor='val_loss', verbose=1, save_best_only=True), reduce_lr]
 
-history = model.fit(X_train, y_train,
+history = model.fit(X_train, y_train[:, 1:].astype(float),  # skip first column since it contains the image_id
                     batch_size=batch_size,
                     epochs=epochs,
-                    validation_data=(X_val, y_val),
+                    validation_data=(X_val, y_val[:, 1:].astype(float)),
                     shuffle=True,
                     callbacks=callbacks)
 
 # Score trained model (note that validation loss is actually the same as the mse
-scores = model.evaluate(X_val, y_val, verbose=1)
+scores = model.evaluate(X_val, y_val[:, 1:].astype(float), verbose=1)
 print('Validation loss:', scores[0])
 print('Validation mse:', scores[1])
 
 # %% score trained model and visualize
 
-train_scores = model.evaluate(X_train, y_train, verbose=1)
-val_scores = model.evaluate(X_val, y_val, verbose=1)
+train_scores = model.evaluate(X_train, y_train[:, 1:].astype(float), verbose=1)
+val_scores = model.evaluate(X_val, y_val[:, 1:].astype(float), verbose=1)
 
 print('Training loss:', train_scores[0], ', training mse: ', train_scores[1])
 print('Validation loss:', val_scores[0], ', validation mse: ', val_scores[1])
@@ -88,7 +91,7 @@ plt.plot(np.arange(1, real_epochs + 1, 1), history.history['mse'], 'g-', label='
 plt.plot(np.arange(1, real_epochs + 1, 1), history.history['val_mse'], 'r-', label='validation')
 plt.xlabel("Epochs")
 plt.ylabel("Mse")
-plt.ylim([0, 1])
+plt.ylim([0, 0.4])
 plt.legend()
 
 plt.subplot(1, 2, 2)
@@ -96,11 +99,41 @@ plt.plot(np.arange(1, real_epochs + 1, 1), history.history['loss'], 'g-', label=
 plt.plot(np.arange(1, real_epochs + 1, 1), history.history['val_loss'], 'r-', label='validation')
 plt.xlabel("Epochs")
 plt.ylabel("Loss")
+plt.ylim([0, 0.4])
 plt.legend()
 
 plt.show()
 
 # %% make predictions
-predictions = model.predict(X_val)
+predictions_val = model.predict(X_val)
 
 # %% TODO: 1) Make functins that checks for each predictions which images are closest (rank them)
+
+
+def rank_images(true_label, predictions, scoring_function='mse'):
+    """
+
+    :param true_label:
+    :param predictions:
+    :param scoring_function:
+    :return:
+    """
+
+    ranking = {}
+    for i in range(len(true_label)):
+        scores_ = {}
+        for j in range(len(true_label)):
+            if scoring_function == 'cosine':
+                score = spatial.distance.cosine(predictions[i, :], true_label[j, 1:].astype(float))
+            elif scoring_function == 'mse':
+                # element wise mse
+                score = ((predictions[i, :] - true_label[j, 1:].astype(float)) ** 2).mean(axis=None)
+            else:
+                print("metric not available, available metrics include mse and cosine")
+            scores_[true_label[j, 0]] = score
+        ranking[true_label[i, 0]] = OrderedDict(sorted(scores_.items(), key=lambda x: x[1]))
+    return ranking
+
+
+# %%
+out = rank_images(true_label=y_val, predictions=predictions_val, scoring_function='cosine')
