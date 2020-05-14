d127c99b1e2ee5ccbc68e38a94af6d41dcce44de
Pieter-Jan <pjinghelbrecht@gmail.com>
2020-05-14 04:00:03 +0200

    updated search egine

--- include/search_engine/main.py
+++ include/search_engine/main.py
@@ -65,7 +65,7 @@ def embed_new_caption(new_caption=None,
 
     # TODO: add functionality to load caption id as a new caption
     if new_caption is None and new_caption_id is not None:
-        all_captions = load_caption_database()
+        all_captions = load_captions()
         new_caption = {new_caption_id: all_captions[new_caption_id]}
 
     # preprocess caption: clean and transform to either w2v or bow
@@ -101,39 +101,80 @@ def preprocess_caption(caption=None,
     return trans
 
 
-#%%
-
 def embed_new_image(new_image=None, image_embedder=None):
 
     # TODO: add functionality to load image id as a new image
     # embedding
     return image_embedder.predict(new_image)
 
-# %%
-def load_image_database(path='include/input/image_features.csv'):
-    """
 
+def prepare_image_database(path='include/input/image_features.csv',
+                           image_embedder=None,
+                           save_dir=None):
+
+    """
     :param path:
     :return:
     """
     database_images = pd.read_csv(path, sep=" ", header=None)
     image_ids = database_images.iloc[:, 0].values
     images = database_images.iloc[:, 1:].values
-    return {
+    embedding = image_embedder.predict(images)
+    database_images = {
         "ID": image_ids,
-        "X": images
+        "X": images,
+        "Embedding": embedding
             }
+    # save
+    if save_dir is not None:
+        if not os.path.exists(save_dir):
+            os.makedirs(save_dir)
+        json.dump(database_images, open(os.path.join(save_dir, "database_images.json"), 'w'))
+    return database_images
 
-# %%
 
-def load_caption_database(load_from_json=False,
+def prepare_caption_database(dir="include/output/data/",
+                             transformer=None,
+                             caption_embedder=None,
+                             clean=True,
+                             stem=True,
+                             min_word_length=2,
+                             unique_only=False,
+                             save_dir=None):
+
+    captions = load_captions(dir_to_read_save=dir)
+    captions_X = preprocess_caption(
+        caption=captions,
+        clean=clean,
+        transformer=transformer,
+        min_word_length=min_word_length,
+        stem=stem,
+        unique_only=unique_only
+    )
+    embedding = caption_embedder.predict(captions_X)
+    database_captions = {
+        "ID": np.array(captions.values()),
+        "X": captions_X,
+        "Embedding": embedding
+    }
+    # save
+    if save_dir is not None:
+        if not os.path.exists(save_dir):
+            os.makedirs(save_dir)
+        json.dump(database_captions, open(os.path.join(save_dir, "database_captions.json"), 'w'))
+    return database_captions
+
+def load_captions(load_from_json=False,
                           path_raw_data="include/input/results_20130124.token",
                           encoding="utf8",
-                          dir_to_read_save="include/output/data/"):
+                          dir_to_read_save=None):
 
     # check if we can read from json format
     if load_from_json and dir_to_read_save is not None:
-        database_captions = json.load(open(dir_to_read_save, 'r'))
+        try:
+            database_captions = json.load(open(os.path.join(dir_to_read_save, "all_data.json"), 'r'))
+        except ValueError:
+            print(f"File 'all_data.json' not found in {dir_to_read_save}")
 
     else:
         # open the file as read only
@@ -157,101 +198,47 @@ def load_caption_database(load_from_json=False,
             # convert description tokens back to string
             caption_desc = ' '.join(caption_desc)
             database_captions[caption_id] = caption_desc
+        # save
         if dir_to_read_save is not None:
+            if not os.path.exists(dir_to_read_save):
+                os.makedirs(dir_to_read_save)
             json.dump(database_captions, open(os.path.join(dir_to_read_save, "all_data.json"), 'w'))
 
     return database_captions
 
 
-def get_batch(iterable, n=1):
-    l = len(iterable)
-    for ndx in range(0, l, n):
-        yield iterable[ndx:min(ndx + n, l)]
-
 
 def rank(distance_metric="L2",
          new_embedding=None,
-         database_X=None,
+         database_embedding=None,
          database_id=None,
-         database_model=None,
-         batch_size=512,
          k=10):
 
-    #assert distance_metric in ["L2", "Hamming"]
-    store_dist = []
-    # loop over in batches (predicting the whole database it to large)
-    for batch in get_batch(list(range(0, len(database_X))), batch_size):
-        database_embedding = database_model(database_X[batch, :])
-        if distance_metric == "L2":
-            dist = norm(database_embedding - new_embedding, ord=2, axis=1)
-        else:
-            pass  # TODO: add hamming distance
+    assert distance_metric in ["L2", "Hamming"]
 
-        # store distance
-        store_dist += dist.flatten().tolist()
+    if distance_metric == "L2":
+        dist = norm(database_embedding - new_embedding, ord=2, axis=1)
+    else:
+        pass  # TODO: add hamming distance
 
     # convert to numpy array
-    store_dist = np.array(store_dist)
     # find lowest indices
-    lowest_idx = np.argpartition(store_dist, kth=range(len(store_dist)))[0:k]
+    lowest_idx = np.argpartition(dist, kth=range(len(dist)))[0:k]
     # get lowest distance
-    lowest_dist = store_dist[lowest_idx].flatten().tolist()
+    lowest_dist = dist[lowest_idx].flatten().tolist()
     # get lowest ids
     lowest_ids = database_id[lowest_idx].flatten().tolist()
     # return in dictionary format
     return dict(zip(lowest_ids, lowest_dist))
-# %%
 
 
 
-# %%
 def plot_images(dic):
-    pass
+    pass  # TODO plot top K images
 
 def print_captions(dic):
-    pass
+    pass  # TODO print top K captions
 
 
-# %% Test
 
-# %%
-caption_test = embed_new_caption(
-    new_caption_id='1067675215.jpg#4',
-    caption_embedder=caption_model_triplet,
-    transformer=bow_triplet_loss
-)
 
-# %%
-database_images = load_image_database()
-
-# %%
-
-# rank images
-ranking_out = rank(
-     new_embedding=caption_test,
-     database_X=database_images['X'],
-     database_id=database_images['ID'],
-     database_model=image_model_triplet,
-     batch_size=512,
-     k=10)
-
-# %% TODO rank captions
-databse_captions = load_caption_database()
-ID_captions = databse_captions.keys()
-#%%
-databse_captions = preprocess_caption(
-    caption=databse_captions,
-    clean=True,
-    transformer=bow_triplet_loss,
-    min_word_length=2,
-    stem=True,
-    unique_only=False
-)
-# %%
-ranking_out = rank(
-     new_embedding=caption_test,
-     database_X=databse_captions,
-     database_id=np.array(ID_captions),
-     database_model=image_model_triplet,
-     batch_size=512,
-     k=10)
